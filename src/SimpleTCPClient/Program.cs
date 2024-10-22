b1caebba28a1766eb20a5123ca0d15125fe79c17
Martin A. Ortega <lynxestudio@outlook.com>
2024-10-22 11:42:36 -0600

    Updates source code

---
+++ src/SimpleTCPClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+
+Console.Clear();
+try
+{
+    TcpClient socket = new TcpClient("127.0.0.1",54321);
+    NetworkStream inputStream = socket.GetStream();
+    using (StreamReader reader = new StreamReader(inputStream))
+    {
+        string? message = reader.ReadLine();
+        Console.WriteLine($"[ Message from server ] = {message} ");
+        Console.WriteLine();
+        inputStream.Close();
+        socket.Close();
+    }
+}
+catch (SocketException socketException)
+{
+    Console.WriteLine($"SocketException: {socketException.Message}");
+}
+catch(Exception exception)
+{
+    Console.WriteLine($"Exception: {exception.Message}");
+}
---
+++ src/SimpleTCPServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Linq.Expressions;
+
+
+int port = 54321;
+TcpListener listener = new TcpListener(IPAddress.Loopback,port);
+try
+{
+    //Start listening for client requests
+    listener.Start();
+    Console.WriteLine($"Waiting for connections on {port}...");
+    TcpClient socket = listener.AcceptTcpClient();
+    NetworkStream outputStream = socket.GetStream();
+    using (StreamWriter writer = new StreamWriter(outputStream))
+    {
+        writer.WriteLine("Hello World to the client!");
+    }
+}
+catch (System.Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally{
+    //stop listening for client requests
+    listener.Stop();
+}
